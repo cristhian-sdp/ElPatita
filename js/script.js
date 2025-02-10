aca2d75c022caa7478e9fdaabe3551d5a2f50b4f
Cristhian Steffano Díaz Piminchumo <steffanod948@gmail.com>
2025-02-10 14:30:07 -0500

    Rediseño
    Archivos adicionales de rediseño:
    - pantalla de carga formato webm.
    - compresión de pdfs.
    - styles.css reducido.
    - script.js reducido.
    - menos imágenes a la cola.
    
    Signed-off-by: Cristhian Steffano Díaz Piminchumo <steffanod948@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,118 +1,47 @@
-// Navegación:
-document.addEventListener("DOMContentLoaded", function () {
-  const nav = document.querySelector("nav");
-  const navLinks = document.querySelectorAll("nav a");
-
-  // Añade la clase 'nav-visible' al elemento nav
-  nav.classList.add("nav-visible");
-
-  // Añade un evento de clic a cada enlace de navegación
-  navLinks.forEach((link) => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
-      nav.classList.remove("nav-visible");
-      nav.classList.add("nav-hidden");
-
-      // Después de 400ms, cambia las clases y redirige manualmente
-      setTimeout(() => {
-        nav.classList.remove("nav-hidden");
-        nav.classList.add("nav-visible");
-        window.location.href = link.href;
-      }, 400); // Tiempo de la animación en milisegundos
-    });
-  });
-});
-
 // Pantalla de carga:
 window.addEventListener("load", function () {
-  const loadingScreen = document.querySelector(".loading-screen");
-  const isInitialLoad = !sessionStorage.getItem("initialLoadDone");
-
-  // Establece el tiempo de espera según si es la carga inicial o no
-  const timeoutDuration = isInitialLoad ? 1700 : 1000;
-
-  // Después del tiempo de espera, oculta la pantalla de carga
-  setTimeout(function () {
-    loadingScreen.style.display = "none";
-    if (isInitialLoad) {
-      sessionStorage.setItem("initialLoadDone", "true");
-    }
-  }, timeoutDuration);
-});
-
-// Modal de la galería:
-document.addEventListener("DOMContentLoaded", function () {
-  const modal = document.getElementById("myModal");
-  const modalImg = document.getElementById("img01");
-  const images = document.querySelectorAll(".menu-image");
-  const span = document.getElementsByClassName("close")[0];
+    const loadingScreen = document.querySelector(".loading-screen");
+    const isInitialLoad = !sessionStorage.getItem("initialLoadDone");
   
-  // Añade un evento de clic a cada imagen del menú
-  images.forEach((img) => {
-    img.addEventListener("click", function () {
-      // Muestra el modal
-      modal.style.display = "block";
-      // Establece la fuente de la imagen del modal
-      modalImg.src = this.src;
-      if (window.innerWidth <= 768) {
-        // Verifica si es un dispositivo móvil y ajusta el margen superior
-        modalImg.style.marginTop = "150px";
+    // Establece el tiempo de espera según si es la carga inicial o no
+    const timeoutDuration = isInitialLoad ? 700 : 600;
+  
+    // Después del tiempo de espera, oculta la pantalla de carga
+    setTimeout(function () {
+      loadingScreen.style.display = "none";
+      if (isInitialLoad) {
+        sessionStorage.setItem("initialLoadDone", "true");
       }
-      // Añade la clase 'modal-open' al body para deshabilitar el scroll
-      document.body.classList.add('modal-open');
-    });
+    }, timeoutDuration);
   });
-  // Cierra el modal cuando se hace clic en el botón de cerrar
-  span.onclick = function () {
-    modal.style.display = "none";
-    modalImg.style.marginTop = "0"; // Restablece el margen superior cuando se cierra el modal
-    document.body.classList.remove('modal-open');
-  };
+
+// Animación de Escritura:
+document.addEventListener("DOMContentLoaded", function() {
+  const heroTextButton = document.querySelector('.hero-text-button');
+  heroTextButton.classList.add('typing-animation');
 });
 
-// Copiar el número de teléfono al portapapeles:
-function copyPhoneNumber(button) {
-  const phoneNumber = "993229832";
-  navigator.clipboard
-    .writeText(phoneNumber)
-    .then(() => {
-      button.classList.add("copied");
-      button.disabled = true;
-      // Después de 3 segundos, elimina la clase 'copied' y habilita el botón
-      setTimeout(() => {
-        button.classList.remove("copied");
-        button.disabled = false;
-      }, 3000);
-    })
-    .catch((err) => {
-      console.error("Error al copiar el número de teléfono: ", err);
-    });
-}
+//Animación del pie de página
+document.addEventListener('DOMContentLoaded', function() {
+  const footer = document.querySelector('.footer-content');
 
-// Copo de nieve:
-function createSnowflake() {
-  const snowflake = document.createElement("div");
-  snowflake.classList.add("snowflake");
+  function checkFooterInView() {
+      const rect = footer.getBoundingClientRect();
+      const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
 
-  // Configuración del tamaño del copo de nieve
-  const size = Math.random() * 4 + 4;
-  snowflake.style.width = `${size}px`;
-  snowflake.style.height = `${size}px`;
+      if (rect.top <= windowHeight) {
+          footer.classList.add('fade-in');
+          window.removeEventListener('scroll', checkFooterInView);
+      }
+  }
 
-  // Configuración de la posición horizontal del copo de nieve
-  snowflake.style.left = `${Math.random() * 100}vw`;
+  window.addEventListener('scroll', checkFooterInView);
+  checkFooterInView();
+});
 
-  // Configuración de la duración de la animación (velocidad de caída)
-  const duration = Math.random() * 4 + 3;
-  snowflake.style.animationDuration = `${duration}s`;
-
-  document.body.appendChild(snowflake);
-
-  // Eliminar el copo de nieve después de que haya caído
-  setTimeout(() => {
-    snowflake.remove();
-  }, duration * 1000); // Multiplicar por 1000 para convertir a milisegundos
-}
-
-// Crear un nuevo copo de nieve cada 200ms
-setInterval(createSnowflake, 200);
+//Animación de la carta
+document.querySelectorAll('.carta-image').forEach(image => {
+  image.addEventListener('click', () => {
+      image.classList.toggle('zoomed');
+  });
+});
